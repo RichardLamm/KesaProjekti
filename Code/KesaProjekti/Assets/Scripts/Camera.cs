41583ccd31dfed973fc11aa94983f551f12b023b
Vili Saura <vili.saura@student.tut.fi>
2018-06-03 14:05:14 +0300

    Added camera movement

---
+++ Code/KesaProjekti/Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+
+    private float xAxis;
+    private float yAxis;
+
+    //The speed at which the camera moves
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// The camera movement is done with the input manager (WASD and arrow keys by default)
+	void Update () {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        transform.position += new Vector3(xAxis, yAxis, 0) * Time.deltaTime * speed;
+    }
+}
