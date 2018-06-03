2b0e6de4254c5393132a42f8c54c14877d60c76d
Vili Saura <vili.saura@student.tut.fi>
2018-06-03 14:46:11 +0300

    Added Scrolling for the camera and renamed the script

--- Code/KesaProjekti/Assets/Scripts/CameraControls.cs
+++ Code/KesaProjekti/Assets/Scripts/CameraControls.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class CameraControls : MonoBehaviour {
 
 
     private float xAxis;
     private float yAxis;
+    public float zAxis;
 
     //The speed at which the camera moves
-    public float speed = 5;
+    public float moveSpeed = 5;
+    public float scrollSpeed = 2;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// The camera movement is done with the input manager (WASD and arrow keys by default)
-	void Update () {
+	void LateUpdate () {
         xAxis = Input.GetAxis("Horizontal");
         yAxis = Input.GetAxis("Vertical");
-        transform.position += new Vector3(xAxis, yAxis, 0) * Time.deltaTime * speed;
+        zAxis = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += new Vector3(xAxis, yAxis, 0) * Time.deltaTime * moveSpeed;        
+        Camera.main.orthographicSize -= zAxis * scrollSpeed;
     }
 }
